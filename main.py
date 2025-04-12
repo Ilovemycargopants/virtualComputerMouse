caeaafbeaa5f8d187c0fadd75fd34e6e5e3fe025
Elijah Waldron <Elijahwaldrons@gmail.com>
2025-04-12 13:39:39 -0400

    Install packages run camera
    installed all packages, created a virtual env, and tested out running a camera through user webcam using OpenCv.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-
-def print_hi(name):
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+import cv2
 
 
+#Opens the camera
+cap = cv2.VideoCapture(1)
+
+#keep going until
+while True:
+    #ignore first value it checks if connection was good (ret), record frame telling us sucessfull connection
+    _, frame = cap.read()
+    #name window, what am i showing
+    cv2.imshow("Virtual Mouse",frame)
+    cv2.waitKey(1)
